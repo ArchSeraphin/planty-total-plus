5b3a82d83e6029442427db1695bf9b3c859ea2e8
ArchSeraphin <trinity.informatique@gmail.com>
2025-07-02 12:18:29 +0200

    Child Init

--- functions.php
+++ functions.php
@@ -2,4 +2,6 @@
 // Enqueue le style parent de manière moderne
 add_action('wp_enqueue_scripts', function() {
     wp_enqueue_style('total-plus-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('total-plus-child-style', get_stylesheet_uri(), array('total-plus-style'));
+
 });
