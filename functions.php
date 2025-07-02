f180cd7608034ac5e96670959c4d2f817ce81aca
ArchSeraphin <trinity.informatique@gmail.com>
2025-07-02 12:45:12 +0200

    Import Font

--- functions.php
+++ functions.php
@@ -3,5 +3,5 @@
 add_action('wp_enqueue_scripts', function() {
     wp_enqueue_style('total-plus-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('total-plus-child-style', get_stylesheet_uri(), array('total-plus-style'));
-
 });
+
