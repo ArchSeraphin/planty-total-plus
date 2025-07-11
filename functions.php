cee6e11ddbba70a2f6a3da784591f057af344062
ArchSeraphin <trinity.informatique@gmail.com>
2025-07-11 10:41:24 +0200

    Uptdate/functions

--- functions.php
+++ functions.php
@@ -8,11 +8,10 @@ add_action('wp_enqueue_scripts', function() {
 add_filter( 'wp_nav_menu_items', 'ajouter_lien_admin_si_connecte', 10, 2 );
 
 function ajouter_lien_admin_si_connecte( $items, $args ) {
-    // Remplacer 'primary' par l'emplacement de ton menu si besoin
     if ( is_user_logged_in() && $args->theme_location == 'primary' ) {
         // URL du panel admin
         $admin_url = admin_url();
-        // Créer l'item
+        // Ajout de l'item admin
         $items .= '<li class="menu-item"><a href="' . esc_url($admin_url) . '">Admin</a></li>';
     }
     return $items;
